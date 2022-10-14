cab001982bc76e0f110cba0295eb7c7efadd933b
MarchettoFrancesco <silver2002.fm@gmail.com>
2022-10-14 10:36:00 +0200

    binary search recursive changed more efficient version and added constructors with comments

--- src/BinarySearch.java
+++ src/BinarySearch.java
@@ -2,42 +2,56 @@ public class BinarySearch {
     //dato A ordinato e k intero trovare k in A O(log n)
     public static void main(String[] args) {
         int[] Aordinato ={1,2,3,4,5,6,7,8,10};
-        System.out.println(searchIteration(Aordinato, 8));
+        System.out.println(BinarySearchMethod(Aordinato, 1));
     }
 
-    /*
-    Binary Search
-    gives the position of a k element in an array, if it isn't in the array returns -1
-     */
-    // RICORSIONE not in place O(logn) but space has the stack of calls may reach logn
-    public static int BinarySearchMethod(int A[], int k){ // A ordinato
+
+    public static boolean BinarySearchMethod(int A[], int k){ // A ordinato
         if (k >= A[A.length/2]){
-            return searchRecursion( A, k, A.length/2, A.length-1 );
+            return Cerca( A, k, A.length/2, A.length-1 );
         } else {
-            return searchRecursion(A, k, 0, A.length / 2);
+            return Cerca(A, k, 0, A.length / 2);
         }
     }
-    public static int searchRecursion(int A[], int k, int a, int b){
-        if (b-a == 1 && k != A[a] && k != A[b]){
-            return -1;
+
+
+    // RICORSIONE not in place O(logn) but space has the stack of calls may reach logn
+    /***
+     * Boolean BinarySearch
+     * @param A
+     * @param k number to search
+     * @param a start index, must be 0<=a<=b
+     * @param b end index
+     * @return false if k isnt in A[0,..,b], otherwise true
+     */
+    public static boolean Cerca(int A[], int k, int a, int b){
+        if (a==b){
+            return (A[a] == k);
         }
-        else if (k == A[(a+b)/2]){
-            return (a+b)/2;
-        } else {
-            if (k > A[(a+b)/2]){
-                return searchRecursion(A,k,(a+b)/2+1,b);
+        else {
+            int r = (a+b)/2;
+            if (A[r] == k){
+                return true;
+            } else if (A[r] > k){
+                return Cerca(A,k,a,r);
             } else {
-                return searchRecursion(A,k,a,(a+b)/2-1);
+                return Cerca(A,k,r+1,b);
             }
         }
     }
 
     // ITERAZIONE inplace O(logn) extra space O(1)
 
+    /***
+     * Position Binary search Iterative
+     * @param A
+     * @param k Number to search
+     * @return position of k if it is in A[0,..A.length-1], -1 otherwise
+     */
     public static int searchIteration(int A[], int k){
         int a = 0, b = A.length-1, media;
 
-        while (a<b){
+        while (a<=b){
             media = (A.length % 2 == 0)?  a+b/2 : (a+b+1)/2;
             if (k == A[media]) {
                 return media;
---
+++ src/MoveToEnd.java
@@ -0,0 +1,29 @@
+public class MoveToEnd {
+    public static void main(String[] args) {
+        int[] array = {1,2,3,4,5,6,7};
+        MovePast(array, 6);
+        for (int i = 0; i<array.length;i++){
+            System.out.println(array[i]);
+        }
+
+    }
+
+    /***
+     *
+     * @param A Array not null
+     * @param i in A[0,n-1]
+     * modifies the array into an array that contains elements from A[i+1] to A[n-1] concatenated with elements
+     *          from 0 to A[i]
+     */
+    public static void MovePast(int A[], int i){
+        int n = A.length;
+        int k = n-i-1,key;
+        while (i >= 0 && n != k){
+            key = A[n-1];
+            A[n-1] = A[i];
+            A[i] = key;
+            i--;
+            n--;
+        }
+    }
+}
